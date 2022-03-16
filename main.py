ba9b3789d3184180111f4f87e28a0d272b8b0799
waitingdodo <953503192@qq.com>
2022-03-16 17:27:17 +0800

    通过配置文件config.json操作

--- main.py
+++ main.py
@@ -8,19 +8,18 @@ import datetime
 
 import os
 
-from dz_utils.dz_utils import dict_set, get_current_time, time_int2str, write_json_file_breakline, dict_get
+from dz_utils.dz_utils import dict_set, get_current_time, time_int2str, write_json_file_breakline, dict_get, read_json_file
 
 
-def get_FileSize(filePath) -> int:
+def get_FileSize(filePath) -> float:
     """
     获取文件的大小,结果保留两位小数, 单位为MB
     :param filePath:
     :return:
     """
     fsize = os.path.getsize(filePath)
-    return fsize
-    # fsize = fsize / float(1024 * 1024)
-    # return round(fsize, 2)
+    fsize = fsize / float(1024 * 1024)
+    return round(fsize, 2)
 
 
 def get_FileAccessTime(filePath):
@@ -88,22 +87,62 @@ def get_file_md5(file_name) -> str:
 
 
 def main():
-    all_info = {}
-    root_dir_path_list = ['D:/', 'C:/']
+    all_info: dict = {}
+    config: dict = read_json_file("./config.json")
+    # 需要执行检查的目录列表
+    root_dir_path_list: list[str] = dict_get(config, "check_dir_path_list", [])
+
+    # 需要忽略的目录列表
+    ignore_dir_list: list[str] = dict_get(config, "ignore_dir_list", [])
+    # 需要执行检查的文件后缀名类型
+    suffix_name_list: list[str] = dict_get(config, "suffix_name_list", [])
     for the_dir in root_dir_path_list:
-        walk(all_info, the_dir)
+        walk(all_info, the_dir, ignore_dir_list, suffix_name_list)
 
     write_json_file_breakline("./file_md5_all_info.json", all_info)
 
-    attention_info = {}
+    attention_info_list = []
     for the_md5, the_file_path_list in all_info.items():
         if len(the_file_path_list) > 1:
-            dict_set(attention_info, the_md5, the_file_path_list)
+            file_size: float = get_FileSize(the_file_path_list[0])
+            attention_info_list.append([the_md5, file_size, the_file_path_list])
+            # dict_set(attention_info, the_md5, the_file_path_list)
+    attention_info_list.sort(key=sort_key, reverse=True)
+    write_json_file_breakline("./file_md5_attention_info.json", attention_info_list)
 
-    write_json_file_breakline("./file_md5_attention_info.json", attention_info)
+
+def sort_key(elem: list):
+    return elem[1]
 
 
-def walk(all_info: dict, root_dir_path: str):
+def is_need_check(file_path: str, ignore_dir_list: list[str], suffix_name_list: list[str]) -> bool:
+    is_right_suffix: bool = False
+    for the_suffix in suffix_name_list:
+        the_suffix = str.lower(the_suffix)
+        if not str.startswith(the_suffix, "."):
+            # 为后缀名添加一个点
+            the_suffix = "." + the_suffix
+
+        file_path_lower = str.lower(file_path)
+        # 转为小写, 便于比对
+        if str.endswith(file_path_lower, the_suffix):
+            is_right_suffix = True
+            break
+
+    if not is_right_suffix:
+        # 如果文件后缀名不符合要求, 则不必检查
+        return False
+
+    for the_ignore_dir in ignore_dir_list:
+        if str.startswith(file_path, the_ignore_dir):
+            # 如果文件处于忽略目录中, 则不必检查
+            return False
+
+    # 文件后缀名符合要求, 不切不在忽略目录中, 则需要检查
+    return True
+
+
+def walk(all_info: dict, root_dir_path: str, ignore_dir_list: list[str], suffix_name_list: list[str]):
     for root, dirs, files in os.walk(root_dir_path):
 
         # root 表示当前正在访问的文件夹路径
@@ -112,10 +151,11 @@ def walk(all_info: dict, root_dir_path: str):
 
         # 遍历文件
         for f in files:
-            is_res: bool = (".jpg" in f) or (".jpeg" in f) or (".png" in f) or (".mp4" in f)
-            if not is_res:
-                continue
+
             a = os.path.join(root, f)
+            if not is_need_check(a, ignore_dir_list, suffix_name_list):
+                # 判定不属于资源文件, 所以不必检查
+                continue
             # print(a, root, f)
             # print(f"os.path.dirname(a):{os.path.dirname(a)}")
             # print(f"os.path.split(a):{os.path.split(a)}")
